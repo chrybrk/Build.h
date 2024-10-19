fe682bc20f71b1f6ddfa80b7e204fa4462af46f4
chrybrk <rahul003chry@gmail.com>
2024-10-19 21:25:43 +0530

    updated few things, not changed much tho.

--- build.h
+++ build.h
@@ -1,87 +1,28 @@
 #ifdef IMPLEMENT_BUILD_C
 
-#if __WIN32__
-#include <errno.h>
-#endif
+/*
+	MIT License
 
-// ------------------------------------------
+	Copyright (c) 2024 Chry003
 
-// Regular text
-#define TBLK "\e[0;30m"
-#define TRED "\e[0;31m"
-#define TGRN "\e[0;32m"
-#define TYEL "\e[0;33m"
-#define TBLU "\e[0;34m"
-#define TMAG "\e[0;35m"
-#define TCYN "\e[0;36m"
-#define TWHT "\e[0;37m"
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
 
-// Regular bold text
-#define BBLK "\e[1;30m"
-#define BRED "\e[1;31m"
-#define BGRN "\e[1;32m"
-#define BYEL "\e[1;33m"
-#define BBLU "\e[1;34m"
-#define BMAG "\e[1;35m"
-#define BCYN "\e[1;36m"
-#define BWHT "\e[1;37m"
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
 
-// Regular underline text
-#define UBLK "\e[4;30m"
-#define URED "\e[4;31m"
-#define UGRN "\e[4;32m"
-#define UYEL "\e[4;33m"
-#define UBLU "\e[4;34m"
-#define UMAG "\e[4;35m"
-#define UCYN "\e[4;36m"
-#define UWHT "\e[4;37m"
-
-// Regular background
-#define BLKB "\e[40m"
-#define REDB "\e[41m"
-#define GRNB "\e[42m"
-#define YELB "\e[43m"
-#define BLUB "\e[44m"
-#define MAGB "\e[45m"
-#define CYNB "\e[46m"
-#define WHTB "\e[47m"
-
-// High intensty background 
-#define BLKHB "\e[0;100m"
-#define REDHB "\e[0;101m"
-#define GRNHB "\e[0;102m"
-#define YELHB "\e[0;103m"
-#define BLUHB "\e[0;104m"
-#define MAGHB "\e[0;105m"
-#define CYNHB "\e[0;106m"
-#define WHTHB "\e[0;107m"
-
-// High intensty text
-#define HBLK "\e[0;90m"
-#define HRED "\e[0;91m"
-#define HGRN "\e[0;92m"
-#define HYEL "\e[0;93m"
-#define HBLU "\e[0;94m"
-#define HMAG "\e[0;95m"
-#define HCYN "\e[0;96m"
-#define HWHT "\e[0;97m"
-
-// Bold high intensity text
-#define BHBLK "\e[1;90m"
-#define BHRED "\e[1;91m"
-#define BHGRN "\e[1;92m"
-#define BHYEL "\e[1;93m"
-#define BHBLU "\e[1;94m"
-#define BHMAG "\e[1;95m"
-#define BHCYN "\e[1;96m"
-#define BHWHT "\e[1;97m"
-
-// Reset
-#define reset "\e[0m"
-#define CRESET "\e[0m"
-#define COLOR_RESET "\e[0m"
-
-// ------------------------------------------
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -90,57 +31,250 @@
 #include <stdarg.h>
 #include <stdbool.h>
 #include <sys/types.h>
-#if __UNIX__
-#include <sys/wait.h>
-#endif
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <limits.h>
 
-#ifndef SOURCE_C_FILE
-#define SOURCE_C_FILE "build.c"
+#if __WIN32__
+	#include <errno.h>
 #endif
 
-#ifndef SOURCE_EXEC_FILE
-#if __UNIX__
-#define SOURCE_EXEC_FILE "build"
-#elif __WIN32__
-#define SOURCE_EXEC_FILE "build.exe"
-#endif
+#if __unix__
+	#include <sys/wait.h>
 #endif
 
-#define safe(value)	__safe_callback__(value, (char*)__FILE__, __LINE__, (char*)__FUNCTION__)
-#define INFO(...)	printf("%s[INFO]:%s %s\n", BGRN, reset, writef(__VA_ARGS__));
-#define WARN(...)	printf("%s[WARN]:%s %s\n", BYEL, reset, writef(__VA_ARGS__));
-#define ERROR(...) printf("%s[ERROR]:%s %s\n", BRED, reset, writef(__VA_ARGS__)), exit(1);
-#define ACTION(...) printf("%s[ACTION]:%s %s\n", BCYN, reset, writef(__VA_ARGS__));
+// If user has not define file for auto compilation
+#ifndef BUILD_SOURCE_FILE
+	#define BUILD_SOURCE_FILE "build.c"
+#endif // BUILD_SOURCE_FILE
+
+#ifndef BUILD_OUTPUT_FILE
+	#if __WIN32__
+		#define BUILD_OUTPUT_FILE "build.exe" // windows
+	#else
+		#define BUILD_OUTPUT_FILE "build"			// linux
+	#endif
+#endif // BUILD_OUTPUT_FILE
+
+#ifndef CMD_DEBUG_OUTPUT
+	#define CMD_DEBUG_OUTPUT true
+#endif // CMD_DEBUG_OUTPUT
+
+typedef enum {
+	BLACK 	= 0,
+	RED 		= 1,
+	GREEN 	= 2,
+	YELLOW 	= 3,
+	BLUE 		= 4,
+	MAGENTA = 5,
+	CYAN 		= 6,
+	WHITE 	= 7
+} TERM_COLOR;
+
+typedef enum {
+	TEXT = 0,
+	BOLD_TEXT,
+	UNDERLINE_TEXT,
+	BACKGROUND,
+	HIGH_INTEN_BG,
+	HIGH_INTEN_TEXT,
+	BOLD_HIGH_INTEN_TEXT,
+	RESET
+} TERM_KIND;
+
+
+/********************************************
+ * 						MACRO FUNCTIONS	
+********************************************/
 #define CMD(...) cmd_execute(__VA_ARGS__, NULL)
 #define writef(...) ({  writef_function(__VA_ARGS__, NULL); })
+#define INFO(...) printf("%s[INFO]:%s %s\n", get_term_color(TEXT, GREEN), get_term_color(RESET, 0), writef(__VA_ARGS__))
+#define WARN(...) printf("%s[WARN]:%s %s\n", get_term_color(TEXT, YELLOW), get_term_color(RESET, 0), writef(__VA_ARGS__))
+#define ERROR(...) printf("%s[ERROR]:%s %s\n", get_term_color(TEXT, RED), get_term_color(RESET, 0), writef(__VA_ARGS__)), exit(1);
 
-bool state_of_rebuilds = false;
+/********************************************
+ * 							DECLARATION
+********************************************/
 
-char *writef_function(char *s, ...);
-void cmd_execute(char *first, ...);
-char *join(unsigned char sep, const char **buffer, size_t n);
-void *__safe_callback__(void *PTR, char *F, int LINE, char *FUNCTION_NAME);
-time_t get_last_modification_time(const char *filename);
-bool needs_recompilation(const char *binary, const char *sources[], size_t num_sources);
-char **get_files_from_directory(const char *path, int *count);
+/*
+ * get_term_color(TERM_KIND, TERM_COLOR)
+ * return terminal color string
+*/
+const char *get_term_color(TERM_KIND, TERM_COLOR);
 
-char **get_files_from_directory(const char *path, int *count)
+/*
+ * writef_function(char *, args) 
+ * It works like printf but it returns string.
+*/
+char *writef_function(char *, ...);
+
+/*
+ * substr(const char*, size_t, size_t)
+ * It will return sub string from point A to B
+*/
+char *substr(const char *, size_t, size_t);
+
+/*
+ * get_list_of_files(const char *, int *)
+ * It will get all of the files in the given path,
+ * (int *) will have the length of the given files.
+ * (char **) return files;
+*/
+char **get_list_of_files(const char *, int *);
+
+/*
+ * get_files_from_directory(const char *)
+ * last modified time of the given file.
+*/
+time_t get_last_modification_time(const char *);
+
+/*
+ * needs_recompilation(const char *, const char *[], size_t)
+ * It will return true if any of the given files
+ * have modification time greater than the binary.
+*/
+bool needs_recompilation(const char *, const char *[], size_t);
+
+/*
+ * join(unsigned char, const char **, size_t)
+ * Adds seperator to the given string list,
+ * and returns one whole string.
+*/
+char *join(unsigned char, const char **, size_t);
+
+/*
+ * cmd_execute(char *, args)
+ * I think the function name is self-explanatory,
+ * it lets you excute commands.
+*/
+void cmd_execute(char *, ...);
+
+/*
+ * strlistcmp(const char *, const char **, size_t)
+ * like `strcmp` it compares two string together,
+ * `strlistcmp` will compare multiple string with one
+ * return true if any of the given files match.
+*/
+bool strlistcmp(const char *, const char **, size_t);
+
+/********************************************
+ * 						   DEFINITION	
+********************************************/
+const char *get_term_color(TERM_KIND kind, TERM_COLOR color)
+{
+	switch (kind)
+	{
+		case TEXT: return writef("\e[0;3%dm", color);
+		case BOLD_TEXT: return writef("\e[1;3%dm", color);
+		case UNDERLINE_TEXT: return writef("\e[4;3%dm", color);
+		case BACKGROUND: return writef("\e[4%dm", color);
+		case HIGH_INTEN_BG: return writef("\e[0;10%dm", color);
+		case HIGH_INTEN_TEXT: return writef("\e[0;9%dm", color);
+		case BOLD_HIGH_INTEN_TEXT: return writef("\e[1;9%dm", color);
+		case RESET: return writef("\e[0m");
+	}
+}
+
+char *writef_function(char *s, ...)
+{
+	// allocate small size buffer
+	size_t buffer_size = 64; // 64 bytes
+	char *buffer = (char*)malloc(buffer_size);
+
+	if (buffer == NULL)
+	{
+		WARN("writef: Failed to allocate buffer.");
+		return NULL;
+	}
+
+	va_list ap;
+	va_start(ap, s);
+
+	int nSize = vsnprintf(buffer, buffer_size, s, ap);
+	if (nSize < 0)
+	{
+		free(buffer);
+		va_end(ap);
+	}
+
+	// if buffer does not have enough space then extend it.
+	if (nSize >= buffer_size)
+	{
+		buffer_size = nSize + 1;
+		buffer = (char*)realloc(buffer, buffer_size);
+
+		if (buffer == NULL)
+		{
+			WARN("writef: Failed to re-allocate buffer.");
+			return NULL;
+		}
+
+		va_end(ap);
+
+		va_start(ap, s);
+		vsnprintf(buffer, buffer_size, s, ap);
+	}
+
+	va_end(ap);
+
+	return buffer;
+}
+
+char *substr(const char *string, size_t n1, size_t n2)
+{
+	if (string == NULL)
+	{
+		WARN("substr: Cannot create substr from NULL.");
+		return NULL;
+	}
+
+	size_t len = strlen(string);
+
+	/*
+	 * n1 and n2 must be greater than 0,
+	 * and n1 must be smaller than n2,
+	 * n2 must be smaller than total length.
+	 *
+	 * Otherwise return NULL;
+	 */
+	if (n1 < 0 && n2 < 0 && n1 >= n2 && n2 >= len)
+	{
+		WARN("substr: Undefined behaviour of `n1` and `n2`.");
+		return NULL;
+	}
+
+	char *result = (char*)malloc((n2 - n1) * sizeof(char));
+	if (result == NULL)
+	{
+		WARN("substr: Failed to allocate buffer.");
+		return NULL;
+	}
+
+	for (size_t i = 0; i < n2 - n1; ++i)
+		result[i] = string[i + n1];
+
+	result[n2 - n1] = '\0';
+
+	return result;
+}
+
+char **get_list_of_files(const char *path, int *count)
 {
 	int internalCounter = 0;
-	char **buffer = (char**)safe(malloc(sizeof(char**) * internalCounter));
+	char **buffer = (char**)malloc(sizeof(char**) * internalCounter);
 
 	DIR *dir = opendir(path);
-	if (dir == NULL) return NULL;
+	if (dir == NULL)
+	{
+		ERROR("Directory `%s` does not exist.", path);
+		return NULL;
+	}
 
 	struct dirent *data;
 	while ((data = readdir(dir)) != NULL)
 	{
-		// it means that the current *ptr is a file not a directory
-#		if __UNIX__
+#		if __unix__
 		if (data->d_type != DT_DIR)
 #		elif __WIN32__
 		if (data->d_ino != ENOTDIR && (strcmp(data->d_name, ".") && strcmp(data->d_name, "..")))
@@ -174,10 +308,7 @@ bool needs_recompilation(const char *binary, const char *sources[], size_t num_s
 {
 	time_t binary_timestamp = get_last_modification_time(binary);
 	if (binary_timestamp == (time_t)(-1))
-	{
-		state_of_rebuilds = true;
 		return true;
-	}
 
 	for (size_t i = 0; i < num_sources; ++i) {
 		time_t source_timestamp = get_last_modification_time(sources[i]);
@@ -187,48 +318,12 @@ bool needs_recompilation(const char *binary, const char *sources[], size_t num_s
 		}
 
 		if (source_timestamp > binary_timestamp)
-		{
-			state_of_rebuilds = true;
 			return true;
-		}
 	}
+
+	INFO("`%s` is already updated.", binary);
 
 	return false;
-}
-
-char *writef_function(char *s, ...)
-{
-	size_t buffer_size = 64; // 64 bytes
-	char *buffer = (char*)malloc(buffer_size);
-
-	if (buffer == NULL) return NULL;
-
-	va_list ap;
-	va_start(ap, s);
-
-	int nSize = vsnprintf(buffer, buffer_size, s, ap);
-	if (nSize < 0)
-	{
-		free(buffer);
-		va_end(ap);
-	}
-
-	if (nSize >= buffer_size)
-	{
-		buffer_size = nSize + 1;
-		buffer = (char*)realloc(buffer, buffer_size);
-
-		if (buffer == NULL) return NULL;
-
-		va_end(ap);
-
-		va_start(ap, s);
-		vsnprintf(buffer, buffer_size, s, ap);
-	}
-
-	va_end(ap);
-
-	return buffer;
 }
 
 char *join(unsigned char sep, const char **buffer, size_t n)
@@ -273,19 +368,11 @@ char *join(unsigned char sep, const char **buffer, size_t n)
 	char *bf = (char*)malloc(ptr_in_pool);
 	bf = strncpy(bf, pool, ptr_in_pool);
 	bf[ptr_in_pool] = '\0'; // don't forget to add null ptr, it is pain in the ass.
+	
+	// we don't need pool because we have already created smaller pool of content size.
+	free(pool);
 
 	return bf;
-}
-
-void *__safe_callback__(void *PTR, char *F, int LINE, char *FUNCTION_NAME)
-{
-	if (PTR == NULL)
-	{
-		fprintf(stdout, "%s:%d :: In %s: \n\tExpected not NULL value, but got NULL.\n", F, LINE, FUNCTION_NAME);
-		exit(EXIT_FAILURE);
-	}
-	
-	return PTR;
 }
 
 void cmd_execute(char *first, ...)
@@ -306,7 +393,6 @@ void cmd_execute(char *first, ...)
 	va_end(args);
 
 	char *buffer[length + 1];
-	safe(buffer);
 
 	length = 0;
 	buffer[length++] = first;
@@ -321,7 +407,10 @@ void cmd_execute(char *first, ...)
 	va_end(args);
 	
 	char *b = join(' ', (const char**)buffer, length);
+
+#if CMD_DEBUG_OUTPUT
 	INFO("CMD: %s", b);
+#endif
 
 	int status = system(b);
 	if (status != 0)
@@ -337,15 +426,26 @@ void cmd_execute(char *first, ...)
 void build_itself() __attribute__((constructor));
 void build_itself()
 {
-	const char *sources[] = { SOURCE_C_FILE, "build.h" };
-	if (needs_recompilation(SOURCE_EXEC_FILE, sources, sizeof(sources) / sizeof(sources[0])))
+	const char *sources[] = { BUILD_SOURCE_FILE, "build.h" };
+	if (needs_recompilation(BUILD_OUTPUT_FILE, sources, sizeof(sources) / sizeof(sources[0])))
 	{
 		INFO("Source file has changed, it needs to be recompiled.");
-		CMD("gcc", SOURCE_C_FILE, "-I.", "-o", writef("%s.new", SOURCE_EXEC_FILE));
-		CMD("mv", SOURCE_EXEC_FILE, writef("%s.old", SOURCE_EXEC_FILE));
-		CMD("mv", writef("%s.new", SOURCE_EXEC_FILE), SOURCE_EXEC_FILE);
+		CMD("gcc", BUILD_SOURCE_FILE, "-I.", "-o", writef("%s.new", BUILD_OUTPUT_FILE));
+		CMD("mv", BUILD_OUTPUT_FILE, writef("%s.old", BUILD_OUTPUT_FILE));
+		CMD("mv", writef("%s.new", BUILD_OUTPUT_FILE), BUILD_OUTPUT_FILE);
+#ifdef __unix__
+		CMD(writef("./%s", BUILD_OUTPUT_FILE));
+#endif
 		exit(0);
 	}
 }
 
-#endif
+bool strlistcmp(const char *s1, const char **s2, size_t n)
+{
+	for (size_t i = 0; i < n; ++i)
+		if (!strcmp(s1, s2[i])) return true;
+
+	return false;
+}
+
+#endif // IMPLEMENT_BUILD_C
